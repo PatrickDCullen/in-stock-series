6737936ff9ff4227a0e7ac7b9dbffe806bceb7a7
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-16 17:17:59 -0500

    End Ep 6: Fake it Till You Make it

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Unit;
 
+use Mockery;
 use Tests\TestCase;
 use App\Models\Stock;
 // use PHPUnit\Framework\TestCase;
+use App\Clients\Client;
 use App\Models\Retailer;
+use App\Clients\StockStatus;
 use App\Clients\ClientException;
+use Facades\App\Clients\ClientFactory;
 use Database\Seeders\RetailerWithProductSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StockTest extends TestCase
 {
     use RefreshDatabase;
+
     /** @test */
-    public function it_throws_an_exception_if_a_client_is_not_found_when_tracking()
+    function it_throws_an_exception_if_a_client_is_not_found_when_tracking()
     {
         $this->seed(RetailerWithProductSeeder::class);
 
@@ -24,4 +29,20 @@ class StockTest extends TestCase
 
         Stock::first()->track();
     }
+
+    /** @test */
+    public function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(
+            new StockStatus($available = true, $price = 9900)
+        );
+
+        $stock = tap(Stock::first())->track();
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
+    }
 }
+
