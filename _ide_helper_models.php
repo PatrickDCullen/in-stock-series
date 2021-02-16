b6d56af1e259447b85a765eb8df87a043cb51fed
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-15 20:25:06 -0500

    End Ep 2: Initial Relationships

---
+++ _ide_helper_models.php
@@ -0,0 +1,112 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Stock[] $stock
+ * @property-read int|null $stock_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ */
+	class Product extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Retailer
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Stock[] $stock
+ * @property-read int|null $stock_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Retailer whereUpdatedAt($value)
+ */
+	class Retailer extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Stock
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property int $retailer_id
+ * @property int $price
+ * @property string|null $sku
+ * @property string $url
+ * @property int $in_stock
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereInStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereRetailerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereSku($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Stock whereUrl($value)
+ */
+	class Stock extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
